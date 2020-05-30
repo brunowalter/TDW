d8993ab9ec996b0389eb33bf438b9db60eefa90f
Bruno Eduardo Procopiuk Walter <48420555+brunowalter@users.noreply.github.com>
2020-05-30 14:31:17 -0300

    atualizacao

--- Bruno/Trabalho01-JSF+CRUD/target/generated-sources/annotations/tdw/entities/Book_.java
+++ Bruno/Trabalho01-JSF+CRUD/target/generated-sources/annotations/tdw/entities/Book_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-30T13:00:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-30T13:54:14")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
